ab052524dc28be1a791dc75cc417e05813da9506
khoabin-star <68915143+khoabin-star@users.noreply.github.com>
2020-12-03 16:04:57 -0600

    Update HelloWorld.java

--- src/unl/cse/HelloWorld.java
+++ src/unl/cse/HelloWorld.java
@@ -7,6 +7,7 @@ public class HelloWorld {
 		System.out.println("How are you ?");
                 System.out.println("I am fine, Nice to meet you!");
 		System.out.println("I am so hungry . Do you have anything to eat ?");
+		System.out.println("Yes I had mixed rice paper. But I ate it without sharing it with my dearest friends");
 	}
 
 }
