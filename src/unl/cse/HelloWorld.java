15049639f7cb773af944a8a7c1e4588b7d1c7950
tunhatthanhthao <69448536+tunhatthanhthao@users.noreply.github.com>
2020-12-03 16:06:29 -0600

    hello Khoa

--- src/unl/cse/HelloWorld.java
+++ src/unl/cse/HelloWorld.java
@@ -8,6 +8,7 @@ public class HelloWorld {
                 System.out.println("I am fine, Nice to meet you!");
 		System.out.println("I am so hungry . Do you have anything to eat ?");
 		System.out.println("Yes I had mixed rice paper. But I ate it without sharing it with my dearest friends");
+		System.out.println("Yes, I am. This is some yummy without sharing with you");
 	}
 
 }
